0c5f032c347b9931b6469a9f068937a7d82d6223
sobadef <sobadef@users.noreply.github.com>
2015-02-21 18:00:18 -0500

    Update topnav.php

--- thefacebook/modules/loggedin/topnav.php
+++ thefacebook/modules/loggedin/topnav.php
@@ -1,10 +1,10 @@
-		            <td bgcolor=#3B5998 valign=bottom>&nbsp;<a class="menu" href='http://beta.thefacebook.xyz/home.php'>home</a></td>
+		            <td bgcolor=#3B5998 valign=bottom>&nbsp;<a class="menu" href='/home.php'>home</a></td>
 
-          <td bgcolor=#3B5998 valign=bottom>&nbsp;<a class="menu" href='http://beta.thefacebook.xyz/search.php'>search</a></td>
+          <td bgcolor=#3B5998 valign=bottom>&nbsp;<a class="menu" href='/search.php'>search</a></td>
 
-          <!--<td bgcolor=#3B5998 valign=bottom>&nbsp;<a class="menu" href="http://beta.thefacebook.xyz/invitefb.php">invite</a></td>-->
+          <!--<td bgcolor=#3B5998 valign=bottom>&nbsp;<a class="menu" href="/invitefb.php">invite</a></td>-->
 		  
-		  <!--<td bgcolor=#3B5998 valign=bottom>&nbsp;<a class="menu" href="http://beta.thefacebook.us/posttowall.php">post&nbsp;to&nbsp;fb</a></td>-->
+		  <!--<td bgcolor=#3B5998 valign=bottom>&nbsp;<a class="menu" href="/posttowall.php">post&nbsp;to&nbsp;fb</a></td>-->
 		  
-		  <td bgcolor=#3B5998 valign=bottom>&nbsp;<a class="menu" href='http://beta.thefacebook.xyz/login.php?logout=1'>logout</a><!--<? echo $sess->Retrieve('logoutUrl'); ?>--></td>
+		  <td bgcolor=#3B5998 valign=bottom>&nbsp;<a class="menu" href='/login.php?logout=1'>logout</a><!--<? echo $sess->Retrieve('logoutUrl'); ?>--></td>
 		  
